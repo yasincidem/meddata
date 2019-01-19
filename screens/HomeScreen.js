206b2f5ef7f91e130d2ab3182ff8264458a06bc1
Yasin <yasincidem@gmail.com>
2019-01-19 21:06:46 +0300

    redundant vars removed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,72 +1,35 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
-  ViewPagerAndroid,
   Button,
   Alert
 } from 'react-native';
-import { WebBrowser, Facebook } from 'expo';
-import fire from '../config/fire';
-import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
-  state = {
-    title: '',
-    arr: []
-  }
-
-  componentDidMount() {
-    console.log('deded');
-    fetch('https://jsonplaceholder.typicode.com/albums')
-    .then((data) => data.json())
-    .then((data) => {
-      this.setState({arr: data})
-      }
-    );
-
-    fire.database().ref().child('name').on('value', (snap) => {
-      this.setState({title: snap.val()});
-    })
-    
-  }
-
   static navigationOptions = {
     header: null,
   };
 
   render() {
-
-    const viewPagerView = this.state.arr.map((value, index) => {
-      return(
-        <View style={styles.pageStyle} key={index + 1}>
-          <Text>{value.title}</Text>
-        </View>
-      );
-    });
-
     return (
       <View style={styles.container}>
         <View>
           <Image
-            style={{width: 100, height: 100}}
+            style={styles.logo}
             source={{uri: 'https://img.icons8.com/color/96/000000/heart-health.png'}}
           />
         </View>
         <View>
           <Text
-            style={{fontSize: 20}}
-            >
+            style={styles.text}>
             MedData
           </Text>
         </View>
 
-        <View style={{marginBottom: 25, marginTop: 150}}>
+        <View style={styles.loginButtonContainer}>
         <Button
           onPress={this.signInWithGoogleAsync} 
           title="Sign in with Google"
@@ -100,9 +63,9 @@ export default class HomeScreen extends React.Component {
       }
     } catch(e) {
       // return {error: true};
+      console.log(e)
     }
   }
-
 
   //Login with Facebook
   async logInWithFacebook() {
@@ -110,9 +73,6 @@ export default class HomeScreen extends React.Component {
       const {
         type,
         token,
-        expires,
-        permissions,
-        declinedPermissions,
       } = await Expo.Facebook.logInWithReadPermissionsAsync('1998838293471317', {
         permissions: ['public_profile'],
       });
@@ -127,135 +87,24 @@ export default class HomeScreen extends React.Component {
       alert(`Facebook Login Error: ${message}`);
     }
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in developm, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
-  viewPager: {
-    flex: 1
-  },
-  pageStyle: {
-    alignItems: 'center',
-    padding: 20,
-  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center'
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
+  logo: {
+    width: 100, 
+    height: 100
   },
-  contentContainer: {
-    paddingTop: 30,
+  text: {
+    fontSize: 20
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  loginButtonContainer: {
+    marginBottom: 25, 
+    marginTop: 150
+  }
 });

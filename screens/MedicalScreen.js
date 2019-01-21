c8519b318d790ac9c4f641c45b0cb9c04ebe0fca
Yasin <yasincidem@gmail.com>
2019-01-21 18:27:45 +0300

    activity screen updated

--- screens/MedicalScreen.js
+++ screens/MedicalScreen.js
@@ -1,19 +1,93 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
-
+import { StyleSheet } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph
+} from 'react-native-chart-kit';
+import {
+  View
+} from 'react-native';
+import fire from '../config/fire';
+import window from '../constants/Layout';
+var moment = require('moment');
 export default class MedicalScreen extends React.Component {
+  state = {
+    labels: [],
+    data: [],
+    commitdata: []
+  }
   static navigationOptions = {
-    title: 'Medical',
+    header: null
   };
 
+  componentDidMount = () => {
+    fire.database().ref().child(`activities/123456`).on('value', (snap) => {
+      const labels = Object.keys(snap.val()).reverse().map((value, index) => {
+        switch(moment(value, 'MM/DD/YYYY').day()) {
+          case 0: return 'Sunday'
+          case 1: return 'Monday'
+          case 2: return 'Tuesday'
+          case 3: return 'Wednesday'
+          case 4: return 'Thursday'
+          case 5: return 'Friday'
+          case 6: return 'Saturday'
+        }
+      })
+      
+      const percentage = Object.values(snap.val()).map((value, index) => {
+				return(Object.values(value[0])[1] * 100);
+      });
+      const keys = Object.values(snap.val()).map((value, index) => {
+				return(Object.keys(snap.val())[index] );
+      });
+      
+      this.setState({labels: keys, data: percentage});
+      
+      // const www = Object.values(snap.val()).map((value, index) => {
+			// 	console.log(Object.keys(snap.val())[index])
+			// 	return(
+			// 		{date: moment(Object.keys(snap.val())[index], 'YYYY-MM-DD').toString(), count: Object.values(value[0])[1] }
+			// 	);
+      // });
+		});
+  }
+  
   render() {
+    // each value represents a goal ring in Progress chart
+    const commitsData = [
+      { date: '2017-01-02', count: 1 },
+      { date: '2017-01-03', count: 2 },
+      { date: '2017-01-04', count: 3 },
+      { date: '2017-01-05', count: 4 },
+      { date: '2017-01-06', count: 5 },
+      { date: '2017-01-30', count: 2 },
+      { date: '2017-01-31', count: 3 },
+      { date: '2017-03-01', count: 2 },
+      { date: '2017-04-02', count: 4 },
+      { date: '2017-03-05', count: 2 },
+      { date: '2017-02-30', count: 4 }
+    ]
+
+    const chartConfig = {
+      backgroundGradientFrom: '#ffffff',
+      backgroundGradientTo: '#ffffff',
+      color: (opacity = 1) => `rgba(27,94,32, ${opacity})`
+    }
+    
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={{marginTop: 32}}>
+      <ContributionGraph
+        values={commitsData}
+        endDate={moment(moment(moment(), 'YYYY-MM-DD'), 'YYYY-MM-DD').toDate()}
+        numDays={105}
+        width={window.window.width}
+        height={220}
+        chartConfig={chartConfig}
+      />
+      </View>
     );
   }
 }
--- screens/RealActivitiesScreen.js
+++ screens/RealActivitiesScreen.js
@@ -10,6 +10,7 @@ import {
 import fire from '../config/fire';
 import Timeline from 'react-native-timeline-listview';
 import { MaterialDialog } from 'react-native-material-dialog';
+import Slider from "react-native-slider";
 var moment = require('moment');
 
 export default class RealActivitiesScreen extends Component {
@@ -17,17 +18,10 @@ export default class RealActivitiesScreen extends Component {
 	state = {
 		data: [],
 		openDialog: false,
-	}
-
-	constructor(props) {
-		super(props);
-		this.data = [
-			{title: moment().format('L'), description: 'Headache'},
-			{title: 'Event 2', description: 'Event 2 Description'},
-			{title: 'Event 3', description: 'Event 3 Description'},
-			{title: 'Event 4', description: 'Event 4 Description'},
-			{title: 'Event 5', description: 'Event 5 Description'}
-		]
+		painPercentage: 0.5,
+		diseases: [],
+		symptoms: [],
+		clickedButtonTitle: ''
 	}
 
 	static navigationOptions = {
@@ -35,50 +29,73 @@ export default class RealActivitiesScreen extends Component {
 	};
 
 	componentDidMount = () => {
-		console.log(moment().format('L').split(new RegExp('/', 'g')).join('-'))
-		fire.database().ref().child(`activities/123456/${moment().format('L').split(new RegExp('/', 'g')).join('-')}/data`).on('value', (snap) => {
-			console.log(snap.val());
+		fire.database().ref().child(`info/123456/diseases`).on('value', (snap) => {
+			this.setState({diseases: snap.val()})
+		});
 
-			// this.setState({arr: snap.val()});
+		fire.database().ref().child(`Symptoms/Asthma`).on('value', (snap) => {
+			this.setState({symptoms: snap.val()})
+			// console.log(snap.val())
+		});
 
-			const items = snap.val().map((value, index) => {
-				return (
-					{title: moment().format('L'), description: value}
+		fire.database().ref().child(`activities/123456`).on('value', (snap) => {
+			// console.log(Object.values(snap.val()));
+			// const items = snap.val().map((value, index) => {s
+			// 	return (
+			// 		{title: moment().format('L'), description: value}
+			// 	);
+			// });
+
+			const www = Object.values(snap.val()).map((value, index) => {
+				console.log(value[0])
+				return(
+					{title: Object.keys(snap.val())[index], description: `${Object.values(value[0])[0]}, ${Object.values(value[0])[1]}  ` }
 				);
 			});
 
-			console.log(items);
-			this.setState({data: items});
+			this.setState({data: www.reverse()});
+
+			// console.log(items);
+			// this.setState({data: items});
 		});
 	}
 	
   render() {
-		// const dates = this.state.arr.map((value, index) => {
-		// 	return (
-		// 		<Text>{value}</Text>
-		// 	);
-		// });
+		
+		const symptomsButton = this.state.symptoms.map((value, index) => {
+			return (
+				<View style={{marginTop: 8}}>
+				<Button
+						onPress={() => {
+							this.setState({openDialog: true, clickedButtonTitle: value});
+						}}
+  					title={value}
+				/>
+				</View>
+			);
+		});
 
     return (
 			<View style={{flex: 1}}>
 				<MaterialDialog
-			  title="Use Google's Location Service?"
-			  visible={this.state.openDialog}
-			  onOk={() => this.setState({ openDialog: false })}
-			  onCancel={() => this.setState({ openDialog: false })}>
-			  <Text>
-			    Let Google help apps determine location. This means sending anonymous
-			    location data to Google, even when no apps are running.
-			  </Text>
+			  	title="How is your abdominal pain now?"
+			  	visible={this.state.openDialog}
+			  	onOk={() => {
+						this.setState({ openDialog: false });
+						const favkey = fire.database().ref().child(`activities/123456/${moment().format().replace(new RegExp('/', 'g'), '-')}`).push().key
+        		const updates = {}
+        		updates[`activities/123456/${moment().format('L').replace(new RegExp('/', 'g'), '-')}`] = {0: this.state.clickedButtonTitle}
+        		fire.database().ref().update(updates)
+					}}
+			  	onCancel={() => this.setState({ openDialog: false })}>
+						<Slider
+          		value={this.state.painPercentage}
+          		onValueChange={painPercentage => this.setState({ painPercentage })}
+        		/>
 				</MaterialDialog>
-				<View style={{marginTop: 48,marginLeft: 16, marginRight: 16, flex: 0.5}}> 
-					<Button
-						style={{marginTop: 48}}
-						onClick={() => {
-							this.setState({openDialog: true});
-						}}
-  					title={'Headache'}
-					/>
+				<Text style={{marginTop: 48,marginLeft: 16, fontWeight: 'bold'}}>{moment().format('L')}</Text>
+				<View style={{marginTop: 8,marginLeft: 16, marginRight: 16, flex: 0.5}}>
+					{symptomsButton}
 				</View>
 				<Timeline
 					innerCircle='dot'
